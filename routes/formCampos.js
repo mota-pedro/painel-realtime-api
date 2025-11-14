845f6b84db3ad01da025fe39d1ad39e82ba7e544
Pedro Mota <pedro.rito@upe.br>
2025-11-14 11:20:49 -0300

    fix: recebendo prpcod nos parametros da rota de listar formCampos por empresa

--- routes/formCampos.js
+++ routes/formCampos.js
@@ -3,7 +3,7 @@ import formCamposController from "../controllers/formCamposController.js";
 export default async function (fastify, opts) {
   fastify.post("/", formCamposController.create);
   fastify.get("/:id", formCamposController.getById);
-  fastify.get("/empresa/:id", formCamposController.listAllByEmpresa);
+  fastify.get("/empresa/:prpcod", formCamposController.listAllByEmpresa);
   fastify.put("/:id", formCamposController.update);
   fastify.delete("/:id", formCamposController.remove);
 }
